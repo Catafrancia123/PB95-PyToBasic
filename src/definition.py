edc0c58a9bf6c0b6d46e563e3ccd7ee21efe2089
normie <aragonisafferz@gmail.com>
2025-02-24 20:43:27 +0700

    Rewrite part 2: CLS and RND, aswell as variables.

--- src/definition.py
+++ src/definition.py
@@ -4,8 +4,30 @@ class Commands:
     def __init__(self):
         self.line_number : int = 1
 
-    def print(self, value):
-        """Default print command found in every programming language."""
-        translate(self.line_number, "print", value)
+    def __str__(self) -> str:
+        return "dear user, please for the love of god dont print the class!!!"
+
+    def print(self, value, var = False):
+        """Displays text on the output, found in every programming language."""
+        if var == True:
+            translate(self.line_number, "print_var", value)
+        else:
+            translate(self.line_number, "print", value)
+        self.line_number += 1
+
+    def clear(self):
+        """Clears the screen. (do we even need this?? its a basic CLS!!!)"""
+        translate(self.line_number, "clear")
+        self.line_number += 1
+    
+    def random(self, end : int):
+        """Generates a random number from 1 to x. (x = end number)"""
+        translate(self.line_number, "random", end)
+        self.line_number += 1
+
+    def let(self, var_name : str, value):
+        """Makes a variable."""
+        value_list = (var_name, value)
+        translate(self.line_number, "variable", value_list)
         self.line_number += 1
         
--- src/translator.py
+++ src/translator.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 # ill add more later - catamapp
 SUPPORTED_FUNCTIONS_OLD = [
     'print', 'input', 'color',
@@ -6,28 +8,52 @@ SUPPORTED_FUNCTIONS_OLD = [
     'plot', 'line', 'circle'
 ]
 
+SUPPORTED_FUNCTIONS_NO_ARGS = {
+    "clear" : "CLS",
+}
 SUPPORTED_FUNCTIONS_ONE_ARGS = {
     "print" : "PRINT",
+    "print_var" : "PRINT",
+    "random" : "RND",
+}
+SUPPORTED_FUNCTIONS_MULTIPLE_ARGS = {
+    "variable" : "LET",
 }
 
 INSTRUCTIONS = {}
 
 def translate(line_number : int, function : str, value = None):
-    function.lower()
-    if function not in SUPPORTED_FUNCTIONS_ONE_ARGS:
+    function = function.lower()
+    if function not in chain(SUPPORTED_FUNCTIONS_NO_ARGS, SUPPORTED_FUNCTIONS_ONE_ARGS, SUPPORTED_FUNCTIONS_MULTIPLE_ARGS):
         raise NotImplementedError("Function not implemented yet, please come back later.")
-    if type(value) == str:
-        value.upper()
-        value = f"\"{value}\""
     
-    if function in SUPPORTED_FUNCTIONS_ONE_ARGS:
+    if type(value) == str and function != "print_var":
+        value = f"\"{value.upper()}\""
+    elif type(value) == str and function == "print_var":
+        value = value.upper()
+
+    if function in SUPPORTED_FUNCTIONS_MULTIPLE_ARGS:
+        INSTRUCTIONS[line_number] = SUPPORTED_FUNCTIONS_MULTIPLE_ARGS[function], value
+        line_number += 1
+    elif function in SUPPORTED_FUNCTIONS_ONE_ARGS:
         INSTRUCTIONS[line_number] = SUPPORTED_FUNCTIONS_ONE_ARGS[function], value
+        line_number += 1
+    elif function in SUPPORTED_FUNCTIONS_NO_ARGS:
+        INSTRUCTIONS[line_number] = SUPPORTED_FUNCTIONS_NO_ARGS[function]
         line_number += 1
 
     with open("output.txt", "w") as file:
-        try:
-            for line, (command, l_value) in INSTRUCTIONS.items():
-                file.write(f"{line:02d} {command} {l_value}\n")
-        except ValueError:
-            for line, (command) in INSTRUCTIONS.items():
+        for line, command in INSTRUCTIONS.items():
+            # special commands
+            if isinstance(command, tuple) and command[0] == "LET":
+                _, (var_name, var_value) = command  # unpack #2
+                file.write(f"{line:02d} LET {var_name.upper()} = {var_value}\n")
+                continue
+
+            # basic handling
+            if isinstance(command, tuple):  # one args
+                l_command, l_value = command
+                file.write(f"{line:02d} {l_command} {l_value}\n")
+            else:  # no args
                 file.write(f"{line:02d} {command}\n")
+        file.close()
